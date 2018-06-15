69a05ff825419c0befdf9f7468dd9980e930a89a
Praveen Codavali <praveencp@yahoo.com>
2018-06-15 10:28:26 +0530

    To test Ansible pull functionality

--- roles/php/files/index.php
+++ roles/php/files/index.php
@@ -4,7 +4,7 @@
 	<title>Welcome to DevOps on AWS</title>
 </head>
 <body>
-	<h1><?php echo "Welcome to DevOps on AWS. This text was generated using PHP 8" ?></h1>
-	
+	<h1><?php echo "Welcome to DevOps on AWS. This text was generated using PHP 7" ?></h1>
+	<h2><?php echo "This is to test ansible pull functionality" ?></h2>
 </body>
 </html>
